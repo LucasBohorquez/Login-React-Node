7f856ae731cf227e29dc9cb7b4a3debddd028858
Lucas <lucasbohorquez30@gmail.com>
2026-03-30 20:39:23 -0500

    contador de clicks1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -51,8 +51,8 @@ export class UserController {
         res
             .cookie('access_token', token, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'lax',
+                secure: true,
+                sameSite: 'none',
             })
             .send({ message: 'Inicio de sesión exitoso', token })
 
