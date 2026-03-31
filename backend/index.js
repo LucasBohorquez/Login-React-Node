b59b04331918355959de9ed4e9f38607d1ba4012
Lucas <lucasbohorquez30@gmail.com>
2026-03-30 20:49:58 -0500

    contador de clicks1

--- backend/index.js
+++ backend/index.js
@@ -16,26 +16,7 @@ app.use(cors({
    origin: (origin, callback) => {
     const allowedOrigins = [
         'http://localhost:5173',
-        'http://localhost:3000',
-        'http://localhost:5173/Login',
-        'http://localhost:5173/Register',
-        'https://login-react-node-production.up.railway.app/',
-        'https://login-react-node-production.up.railway.app/Users',
-        'https://login-react-node-production.up.railway.app/Register',
-        'https://login-react-node-production.up.railway.app/Login',
-        'https://login-react-node-production.up.railway.app/me',
-        'https://login-react-node-production.up.railway.app/Logout',
-        'https://login-react-node-production.up.railway.app/User/:id',
-        'https://login-react-node-production.up.railway.app/User/:id',
-        'https://login-react-node-production.up.railway.app/Recover',
-        'https://login-react-node-production.up.railway.app/Pin',
-        'https://login-react-node-production.up.railway.app/User/:id',
-        'https://login-react-node-alpha.vercel.app/Login',
-        'https://login-react-node-alpha.vercel.app/Register',
-        'https://login-react-node-alpha.vercel.app/Dashboard',
-        'https://login-react-node-alpha.vercel.app/Update',
-        'https://login-react-node-alpha.vercel.app/Delete',
-        'https://login-react-node-alpha.vercel.app/Recover',
+        'https://login-react-node-alpha.vercel.app/'
     ]
     if(allowedOrigins.includes(origin)){
         return callback(null, true)
